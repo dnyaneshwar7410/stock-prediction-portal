d97fa887707f7818aba62d324427bf070280b5b8
dnyaneshwar <dnyaneshwarparkhe2022@gmail.com>
2026-01-23 21:30:14 +0530

    Header,Main,Footer

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "./Button";
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="p-5 text-center bg-light-dark rounded">
+          <h1 className="text-light">Stock Prediction Portal</h1>
+          <p className="text-white lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
+            quisquam quas vitae exercitationem dignissimos perspiciatis, totam,
+            assumenda neque delectus at optio adipisci eligendi ab voluptas
+            recusandae? Soluta ratione blanditiis vero.
+          </p>
+       <Button  text = 'login'  class='btn-outline-warning' />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
