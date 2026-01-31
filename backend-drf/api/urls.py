62a994b0ac6873ca33d655123c6ab7ded8bb5e40
dnyaneshwar <dnyaneshwarparkhe2022@gmail.com>
2026-01-31 19:06:22 +0530

    Integrating Machine Learning Model with Django & React

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts import views as UserViews
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  #for login
+from .views import StockPredictionAPIView
 urlpatterns = [
     # register
     path('register/', UserViews.RegisterView.as_view(), name='register'),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected-view/', UserViews.ProtectedView.as_view()),
+
+    # Prediction API
+    path('predict/',StockPredictionAPIView.as_view(), name='stock_prediction')
 ]
--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,3 +1,142 @@
 from django.shortcuts import render
-
+from .serializers import StockPredictionSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+from django.conf import settings
+from .utils import save_plot
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
+from sklearn.metrics import mean_squared_error, r2_score
 # Create your views here.
+
+class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serialzer = StockPredictionSerializer(data=request.data)
+        if serialzer.is_valid():
+            ticker = serialzer.validated_data['ticker']
+
+            # Fetch the data from yfinance
+
+            now = datetime.now()
+            start = datetime(now.year - 10, now.month, now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+
+            if df.empty:
+                return Response({"error":"No data found for the given ticker", 'status':status.HTTP_404_NOT_FOUND})
+            
+            df = df.reset_index()
+
+            # Generate Basic Plot
+            plt.switch_backend("AGG")
+            plt.figure(figsize=(12, 6))
+            plt.plot(df.Close, label='Closing Price')
+            plt.title(f"Closing Price Of {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+            # Save the plot
+            plot_img_path = f"{ticker}_plot.png"
+            plot_img = save_plot(plot_img_path)
+
+            # 100 Days Moving Average
+            plt.switch_backend("AGG")
+            ma100 = df.Close.rolling(100).mean()
+            plt.figure(figsize=(12, 6))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100,'r', label='100 Days Moving Average')
+            plt.title(f"100 Days Moving Average {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+            plot_img_path = f"{ticker}_100_dma.png"
+            plot_100_dma = save_plot(plot_img_path)
+
+            #  200 Days Moving Average
+            plt.switch_backend('AGG')
+            ma200 = df.Close.rolling(200).mean()
+            plt.figure(figsize=(12,6))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100,'r', label='100 Days Moving Average')
+            plt.plot(ma200, 'g',label='200 Days Moving Average')
+            plt.title(f"200 Days Moving Average {ticker}")
+            plt.xlabel("Days")
+            plt.ylabel("Price")
+            plt.legend()
+            plot_img_path = f"{ticker}_200_dma.png"
+            plot_200_dma = save_plot(plot_img_path)
+
+
+            # Spliting data into Training & Testing datasets
+            data_training = pd.DataFrame(df.Close[0:int(len(df) * 0.7)])
+            data_testing = pd.DataFrame(df.Close[int(len(df) * 0.7) : int(len(df))])
+
+            # Scaling down the data between 0 and 1
+            scaler = MinMaxScaler(feature_range=(0,1))
+
+            # Load ML Model
+            model = load_model('stock_prediction_model.keras')
+
+            # Preparing test data
+            past_100_days = data_training.tail(100)
+            final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+            input_data = scaler.fit_transform(final_df)
+
+            x_test = []
+            y_test = []
+            for i in range(100, input_data.shape[0]):
+                x_test.append(input_data[i-100:i])
+                y_test.append(input_data[i, 0])
+
+            x_test, y_test = np.array(x_test), np.array(y_test)
+
+
+            # Making Predictions
+            y_predicted = model.predict(x_test)
+
+            # Revert the scaled price to original price
+            y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1)).flatten()
+            y_test = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
+
+
+            # Plot the final prediction
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12,6))
+            plt.plot(y_test,'b', label='Original Price')
+            plt.plot(y_predicted,'r', label='Predicted Price')
+            plt.title(f"Final Prediction {ticker}")
+            plt.xlabel("Days")
+            plt.ylabel("Price")
+            plt.legend()
+            plot_img_path = f"{ticker}_final_prediction.png"
+            plot_prediction = save_plot(plot_img_path)
+
+
+            # Model Evaluation
+            # Mean Squared Error (MSE)
+            mse = mean_squared_error(y_test, y_predicted)
+
+            # Root Mean Squared Error (RMSE)
+            rmse = np.sqrt(mse)
+
+            # R-Squared
+            r2 = r2_score(y_test, y_predicted)
+
+
+            return Response({
+                'status':'success',
+                'plot_img':plot_img,
+                'plot_100_dma':plot_100_dma,
+                'plot_200_dma':plot_200_dma,
+                'plot_prediction':plot_prediction,
+                'r2': r2,
+                'mse':mse,
+                'rmse':rmse,
+                })
